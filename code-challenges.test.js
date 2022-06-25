e8802cb803580f6b93088e975cdc997506e73207
scharlez <stevencharlez@gmail.com>
2022-06-25 15:04:48 -0700

    Completed Week 2 Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,9 +16,15 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// describe("functionName", () => {
+//     it("describes what my test and function does", () => {
+//       expect(functionName("pass any params needed")).toEqual("Expect output")
+//     })
+//  })
+
+// const numbersArray1 = [6, 7, 8, 9, 10]
+// // Expected output: [18, 21, 24, 27, 30]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
 
@@ -28,6 +36,18 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// describe(evenDivisiblebyThree, () => {
+//     it("takes a number as an argument and decides if the number is evenly divisble by three or not",()=> {
+//         expect(evenDivisiblebyThree(15)).toEqual("15 is divisible by three")
+//         expect(evenDivisiblebyThree(0)).toEqual("0 is divisible by three")
+//         expect(evenDivisiblebyThree(-7)).toEqual("-7 is not divisible by three")
+//     })
+// })
+
+ //* RAN TEST (RESULTS BELOW): *//
+
+// ReferenceError: evenDivisiblebyThree is not defined
+
 const num1 = 15
 // Expected output: "15 is divisible by three"
 const num2 = 0
@@ -38,25 +58,95 @@ const num3 = -7
 
 // b) Create the function that makes the test pass.
 
+// Pseudocode: 
+// Declare a function called evenDivisiblebyThree.
+// Set parameters referenced as num1, num2, num3. 
+// Use conditional to evalute which inputs are are divisible by three.
+// Return the specific numbers that are divisible and which ones aren't divisible by three. 
 
+// const evenDivisiblebyThree = (num1, num2, num3) => {
+//     if (num1 % 3==0) {
+//         return "15 is divisible by three"
+//     }   else if (num2 % 3==0){
+//         return "0 is divisible by three"
+//     }   else if (num3 % 3!==0){
+//         return "-7 is not divisible by three"
+//     }
+// }
+
+ //* RAN TEST (RESULTS BELOW): *//
+
+// FAIL  ./code-challenges.test.js
+// ● Test suite failed to run
+
+//   ReferenceError: Cannot access 'evenDivisiblebyThree' before initialization
+
+// Note to Instructor: I looked up the error and according to my research this error occurs becuase the the function set up is trying to access the variable before it's initialised so I tried restructuring my code but was getting syntax errors so I'ma bit lost because it looks correct to me. Anyway, this is why I have commented out my #1 - code lines, so I can still run tests for the other function problems.
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// describe(arrayofCapitalization, () => {
+//     it("takes in an array of words and returns an array with all the words capitalized", () => {
+//       expect(arrayofCapitalization("randomNouns1")).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//       expect(arrayofCapitalization("randomNouns2")).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+//     })
+//  })
+
+ //* RAN TEST (RESULTS BELOW): *//
+
+ // ReferenceError: arrayofCapitalization is not defined
 
 const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
-
 // b) Create the function that makes the test pass.
 
+// Pseudocode: 
+// Declare a function called arrayofCapitalization.
+// Set parameters allwordNouns
+// Use conditional to convert array into a on with all capitalization
+    // Use .join to convert to string
+    // Use .charAt(0) && .toUpperCase() to capitalize entire string
+    // Use .split to convert string back to array once capitalized
+// Return output is an array containing all capitalized words.
+
+// const arrayofCapitalization = (array) => {
+//     return array.join(randomNouns1,randomNouns2) + array.charAt(0).toUpperCase +array.split (" ")
+    
+//     }
+    
+   
+// }
+
+//* RAN TEST (RESULTS BELOW): *//
+
+// FAIL  ./code-challenges.test.js
+// ● Test suite failed to run
+
+//   Jest encountered an unexpected token
+
+//   Details:
+
+//   SyntaxError:
+
+// Note to Instructor: I'm at a lost here with this problem, I don't even understand why I'm getting a syntax error (on line 123) or how to correctly use/order these commands. It feels like this entire problem is in Chinese. Literally spent hours on it even though there's not much code. I really thought I was understanding Jest but I haven't even got one of these tests to come back clear and I'm not sure why. This explains why I have commented out my #2 - code lines, so I can still run tests for the other function problems.
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe(pickupVowel, () => {
+    it("takes in a string and logs the index of the first vowel", () => {
+      expect(pickupVowel("vowelTester1")).toEqual(1)
+      expect(pickupVowel("vowelTester2")).toEqual(0)
+      expect(pickupVowel("vowelTester3")).toEqual(2)
+    })
+ })
 
 const vowelTester1 = "learn"
 // Expected output: 1
@@ -65,5 +155,38 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+//* RAN TEST (RESULTS BELOW): *//
+
+// ReferenceError: pickupVowel is not defined
 
 // b) Create the function that makes the test pass.
+
+// Pseudocode: 
+// Declare a function called pickupVowel.
+// Set parameters of string
+// Use conditional to evalute string.
+// Return index of first vowel location. 
+
+// const pickupVowel = (vowels) => {
+//    return vowels.search(a,e,i,o,u)
+// }
+
+// const pickupVowel = (vowels) => {
+//    return vowels.fitler(a,e,i,o,u)
+//    return 1 || 0 || 2
+// }
+
+const pickupVowel = (vowels) => {
+    for (int i =0; i<vowelTester.length, i++) {
+        if ()
+    }
+    }
+
+//* RAN TEST (RESULTS BELOW): *//
+
+// FAIL  ./code-challenges.test.js
+// ● Test suite failed to run
+
+//   ReferenceError: Cannot access 'pickupVowel' before initialization
+
+// Note to Instructor: I was a bit unsure on how to go about this one through the Jest nomenclature. Found this one a bit frustrating because I initially tried using something I found in an online search but felt it was missing something, so I tried .filter then remembered its not an array, so then I started to do a for loop for it but I was unsure of the syntax and how to fill it out with the Jest variables. 
--- interview-questions.md
+++ interview-questions.md
@@ -8,41 +8,39 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer: Parameter can be viewed as a variable that set to define something in a function. While an argument can be seen as a value of that function. So when you return an output it will be a combination of what commands or built in methods were used in tandem with the agrument(s) but are defined by the parameters. 
 
-  Researched answer:
+  Researched answer: Parameters are the variables or the collection of "names" being used or listed within a function that defines it. These names are customized to the function and it is best to keep them reflective on the actions being done for future reference, transparency, and pair programming.  The arguments from a function are the real values that are executed to the function when the active lines of code are invoked. Due to these being used during invocation, one will see the built in methods or commands being used side-by-side. Parameters and arguments are different but both are necessary to have a successful and functional code. 
 
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: The JavaScript built in method .map() has predefined parameters like values, indexes, and arrays. These all can be used together or seperately but at least one value must be used for the function to run this built in method successfully. 
 
-  Researched answer:
+  Researched answer: The JavaScript built in method .map() requires a function defintion/argument as its initial parameter meaning there must at least be a value provided within the designated parentheses for the code to run correctly. However, an index or additional array can be used together with the previously mentioned requirement (value) as optional additions to the function. 
 
 
 
 3. What is the difference between map and filter?
 
-  Your answer:
+  Your answer: The map method uses the original content of the array and produces the same array just after it has undergone a function/action to it. While the filter method is similiar because it takes the same array but with its functionality edits out certain elements within an array based on the parameters set. 
 
-  Researched answer:
-
+  Researched answer: Both the map and the filter methods focus primarily the with array and will produce an output that is essentially an updated version of the original array. The difference is with the map method will be altering each element within the array but still return the same length of that array. Conversely, with the filter method inspect each element of an array and based on the parameters given it will eliminate certain ones in the output and thus create an array that is shorter in length than the original. 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: A function is lines of code written in a text editor to perform certain tasks. A method is a type of function that deals exclusively with objects. Function and method have an interwined relationship because a method will always be a function but a function doesn't necessarily have to be a function; similiar to a square is a type of rectangle but not all rectangles are squares. 
 
-  Researched answer:
-
+  Researched answer: A function is a sequence of written code that are designed to perform specific sets of tasks. It is defined by a function keyword followed and optional parameters (depending on the circumstance) and is encapsulated by Curly brakets. Functions can be associated with objects or not, however when an object is involved this type of function is known as a method. With a method contains that can be identified by the name of its object and and its method name (specific name given that often defines the task action) through the use of dot notation or square braket notation. Lastly, just as classical is a type of music but not all music of course, method is simply one type of function.
 
 
 5. What is object destructuring?
 
-  Your answer:
+  Your answer: Object destructuring is breaking down an object into specific output. So if you had a function with that contained alot of personal informatin like names, address, emails, etc and need to limit all that to just one quick output of a particular name and/or address, you would log that in and the return should produce just that information. 
 
-  Researched answer:
+  Researched answer: Object destructuring is a unique way of redefining the available variables; meaning it allows the programmer to grab objects or arrays and break down the connected values into individualized variables. This is useful because when comes to nested properties, you end up saving a lot of time by using object destructuring due to being able to efficiently alter and pull object properties. 
 
 
 
@@ -58,12 +56,22 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance: recycles the previously used methods and properties of an already created class when you wish to make a new one. 
 
-2. React:
+2. React: open-source front-end Javascript library used by developers for creating user interfaces centered around components and viewable through a local web Browser. 
 
-3. React state:
+3. React state: within React its when an object that stores information that is suspetible to be changed at later point during the life span of a component. 
 
-4. React lifecycle methods:
+4. React lifecycle methods: Consist of 3 main stages --> mounting, updating and then unmounting. 
 
-5. DOM:
+* constructor() - utilized during the mouting, used with every new component creation.
+
+* render() - utilized during the mouting stage & updating stages, a class method for creating and booting up an object's presence within a class. 
+
+* componentDidMount() - utilized during the mouting, used after a component as been update within the DOM. 
+
+* componentDidUpdate() - utilized during the mouting stage & updating stages,used to invoke code after a component has been updated. 
+
+* componentWillUnmount() - utilized during the unmouting stage, used to invoke code when a component somehow gets destroyed or unmounted from the DOM. 
+
+5. DOM: Stands for Document Object Model, is a programming interface for HTML documents used to open and edit documents as it is way to bring up webpage in a structured way. 
